3fdc065cc6a02b4f297b23ff173b646dea3f6ec0
almadorashop <almadora.shop@gmail.com>
2026-05-22 20:07:11 +0100

    Bookify Update

--- src/App.jsx
+++ src/App.jsx
@@ -47,7 +47,7 @@ export default function App() {
         <Route
           path="/admin"
           element={
-            <ProtectedRoute roles={['owner', 'manager', 'cashier', 'waiter', 'kitchen', 'superadmin']}>
+            <ProtectedRoute>
               <AdminLayout />
             </ProtectedRoute>
           }
